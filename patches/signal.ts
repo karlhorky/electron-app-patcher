24d4fdc847a6e2eafa3b7cad1f05032a3e3b92c8
Karl Horky <karl.horky@gmail.com>
2022-10-30 22:52:41 +0100

    Improve formatting of patch

--- patches/signal.ts
+++ patches/signal.ts
@@ -4,39 +4,41 @@ export const patchConfig: PatchConfig = {
   resourcesPath: '/Applications/Signal.app/Contents/Resources',
   transforms: [
     // Patch to focus message composition input when key is pressed
+    // PR: https://github.com/signalapp/Signal-Desktop/pull/4998
     {
       filePath: 'preload.bundle.js',
       transform: (content) => {
         return content.replace(
-          /(\}, \[setLarge\]\);\n {6})(if \(isBlocked \|\| areWePending \|\| messageRequestsEnabled && !acceptedMessageRequest\) \{)/,
+          /(\}, \[setLarge\]\);\n)( {6}if \(isBlocked \|\| areWePending \|\| messageRequestsEnabled && !acceptedMessageRequest\) \{)/,
           `$1
-          (0, import_react.useEffect)(() => {
-            const handler = (e) => {
-              const { key } = e;
+      // Patch to focus message composition input when key is pressed
+      (0, import_react.useEffect)(() => {
+        const handler = (e) => {
+          const { key } = e;
 
-              if (key.length !== 1) {
-                return;
-              }
+          if (key.length !== 1) {
+            return;
+          }
 
-              inputApiRef.current?.focus();
-              const panels = document.querySelectorAll('.conversation .panel');
-              if (panels && panels.length > 1) {
-                return;
-              }
+          inputApiRef.current?.focus();
+          const panels = document.querySelectorAll('.conversation .panel');
+          if (panels && panels.length > 1) {
+            return;
+          }
 
-              if (document.activeElement?.nodeName.toLowerCase() === 'input') {
-                return;
-              }
+          if (document.activeElement?.nodeName.toLowerCase() === 'input') {
+            return;
+          }
 
-              inputApiRef.current?.focus();
-            };
+          inputApiRef.current?.focus();
+        };
 
-            document.addEventListener('keydown', handler);
-            return () => {
-              document.removeEventListener('keydown', handler);
-            };
-          });
-            $2`,
+        document.addEventListener('keydown', handler);
+        return () => {
+          document.removeEventListener('keydown', handler);
+        };
+      });
+$2`,
         );
       },
     },
